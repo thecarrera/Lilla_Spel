4c0d08a28566a8062097697654b70e633715c80d
Eric <erichallengren@live.se>
2017-04-14 12:49:47 +0200

    La in lite funktioner, inget händer än

--- Lilla_Spel/Lilla_Spel/ParticleClass.cpp
+++ Lilla_Spel/Lilla_Spel/ParticleClass.cpp
@@ -1,16 +1,111 @@
 #include "ParticleClass.h"
 
-ParticleClass::ParticleClass(ID3D11Device* gDevice)
+ParticleClass::ParticleClass(ID3D11Device* gDevice, ID3D11DeviceContext* gDeviceContext)
 {
 	this->gDevice = gDevice;
+	this->gDeviceContext = gDeviceContext;
 }
 
-ParticleClass::ParticleClass(ID3D11Device* gDevice, const ParticleClass& particleClass)
+ParticleClass::ParticleClass(ID3D11Device* gDevice, ID3D11DeviceContext* gDeviceContext, const ParticleClass& particleClass)
 {
 	this->gDevice = gDevice;
+	this->gDeviceContext = gDeviceContext;
 }
 
 ParticleClass::~ParticleClass()
 {
 
+}
+
+bool ParticleClass::initialize(WCHAR* textureFileName)
+{
+	bool result = true;
+
+	//texture för partiklar 
+	result = loadTexture(this->gDevice, textureFileName);
+	if (!result)
+	{
+		return false;
+		//filen fanns inte
+	}
+
+	//partiklesystem initialize
+	result = initializeParticleSystem();
+	if (!result)
+	{
+		return false;
+		//partikelsystemet
+	}
+
+	//partikel buffer
+	result = initializeBuffers(this->device);
+	if (!result)
+	{
+		return false;
+	}
+
+	//lyckades med allt
+	return true;
+}
+
+void ParticleClass::kill()
+{
+	//bort med buffer, är tydligen en sak?
+	shutdownBuffers();
+
+	//bort med partikelsystemet
+	shutdownParticleSystem();
+
+	//bort med texturen
+	releaseTexture();
+}
+
+bool ParticleClass::frameUpdate(float frameTime)
+{
+	bool result = true;
+
+	//bort med gamla partiklar
+	killParticles();
+
+	//nya partiklar
+	emitParticles(frameTime);
+
+	//updatera positionen av partiklarna
+	updateParticles(frameTime);
+
+	//måste uppdatera buffern om uppdaterar positionen
+	result = updateBuffers(this->gDeviceContext);
+	if (!result)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void ParticleClass::render()
+{
+	//vertex & index buffern ska uppdateras, vi gör det härifrån
+	renderBuffers(this->gDeviceContext);
+}
+
+void ParticleClass::createTexture()
+{
+	//HRESULT hr = DirectX::CreateWICTextureFromFile(this->gDevice, this->gDeviceContext, L"../resource/Maps/nånting.png", &this->textureResoure, &this->textureView);
+	HRESULT hr = DirectX::
+	if (FAILED(hr))
+	{
+		MessageBox(0, "texture creation failed", "error", MB_OK);
+	}
+}
+
+
+
+
+
+
+
+int ParticleClass::getmIndexCount()
+{
+	return this->mIndexCount
 }
--- Lilla_Spel/Lilla_Spel/ParticleClass.h
+++ Lilla_Spel/Lilla_Spel/ParticleClass.h
@@ -8,15 +8,58 @@
 class ParticleClass
 {
 private:
+	//devices
 	ID3D11Device* gDevice;
+	ID3D11DeviceContext* gDeviceContext;
+
+	//partikel randomizer grejer
+	float mParticleDeviationX;
+	float mParticleDeviationY;
+	float mParticleDeviationZ;
+	float mParticleVelocity;
+	float mParticleVelocityVariation;
+	float mParticleSize;
+	float mParticlesPerSecond;
+	int mMaxParticles;
+
+	//counters
+	int amountOfParticlers;
+	float accumulatedTime;
+
+	//texture
+	ID3D11Resource* textureResoure;
+	ID3D11ShaderResourceView* textureView;
+
+	//array av partiklar
+	ParticleType* particleArray;
+
+	//info om verticerna
+	int mVertexCount;
+	int mIndexCount;
+	vertexType* mVertices;
+
+	//buffers
+	ID3D11Buffer* mVertexBuffer;
+	ID3D11Buffer* mIndexBuffer;
+
 
 public:
-	ParticleClass(ID3D11Device* gDevice);
-	ParticleClass(ID3D11Device* gDevice, const ParticleClass& particleClass);
+	ParticleClass(ID3D11Device* gDevice, ID3D11DeviceContext* gDeviceContext);
+	ParticleClass(ID3D11Device* gDevice, ID3D11DeviceContext* gDeviceContext, const ParticleClass& particleClass);
 	~ParticleClass();
 
-	bool initialize(ID3D11Device* gDevice, WCHAR* hmm);
-	//kolla vad WCHAR var för nåt
+	bool initialize(WCHAR* textureFileName);
+	void kill();
+	bool frameUpdate(float frameTime); // ska vi ha frame time?
+	void render();
+	//function för return texture
+	void createTexture();
+
+
+
+
+	int getmIndexCount();
+
 };
 
-#endif // !
+#endif 
